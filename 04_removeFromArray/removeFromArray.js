39078d8568ff7228acde479e63f2fe11a6c56289
lupusalba <a145296@gmail.com>
2021-11-02 20:46:12 +0100

    finished 4th excersise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,24 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
 
+    let myArray = arr;
+    let myArgs = args;
+    let newArr = [];
+
+
+        for (let i = 0; i < myArray.length; i++) {
+            newArr.push(myArray[i]);
+            for (let j = 0; j < myArgs.length; j++){
+                if (myArray[i] === myArgs[j]){
+                    newArr.pop(myArray[i]);
+                    continue;
+                } else {
+                    continue;
+                }
+            }
+        }
+        return newArr;
+    
+    
 };
 
 // Do not edit below this line
