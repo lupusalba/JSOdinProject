535ec8c6ad105f6a460e1c645119cfd41aed4347
lupusalba <a145296@gmail.com>
2021-11-02 13:12:14 +0100

    compleated exersise 01-03

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,17 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let result = string;
 
+    if (num < 0 ){  return 'ERROR'  }
+    else if(num === 0){ return '';  }
+    else if (num === 1){  return string;  } 
+    else if (num > 1){
+        for (var i = 1; i <num; i++) {
+            result += string;
+        };
+    }
+    return result;
 };
+
 
 // Do not edit below this line
 module.exports = repeatString;
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,12 @@
 const reverseString = function(str) {
+    let arr = str.split("");
+    let temp = [];
+
+    for (let i = arr.length - 1; i >= 0; i--) {
+        temp.push(arr[i]);
+    }
+
+    return temp.join("");
 
 };
 
