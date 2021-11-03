5da3bf2f0fb2a9b361d4a25881cc934aaf9d7be8
lupusalba <a145296@gmail.com>
2021-11-03 18:34:59 +0100

    finished 7th excersise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(deg) {
+  temp =  (( deg - 32 ) * (5/9)).toFixed(1);
+  temp.toFixed;
+  return parseFloat(temp);
 };
 
-const ctof = function() {
-
+const ctof = function(deg) {
+  temp = (deg * (9/5) + 32).toFixed(1);
+  temp.toFixed;
+  return parseFloat(temp);
 };
 
 // Do not edit below this line
