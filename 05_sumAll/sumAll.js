e8d195a7c48bcdddb9885acc991ccb9f30421015
lupusalba <a145296@gmail.com>
2021-11-03 14:58:28 +0100

    finished 5th excersise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,27 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let sum = 0;
 
+    if(typeof num1 === "number" && typeof num2 === "number" && num1 >= 0 && num2>= 0){
+        if(num1 === num2) {
+            sum = num1 + num2;
+        }
+        else if(num1 > num2) {
+            for (let i = num2; i <= num1; i++) {
+                sum += i;
+            }
+        }
+        else if(num1 < num2) {
+            for (let i = num1; i <= num2; i++) {
+                sum += i;
+            }
+        }
+        return sum;
+    }
+    else {
+        return "ERROR";
+    }
+    
+    
 };
 
 // Do not edit below this line
