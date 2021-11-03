51fe1f6c99fa1ee74be0477c50151497d0bbab21
lupusalba <a145296@gmail.com>
2021-11-03 19:10:19 +0100

    finished 8th excersise - Calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let multi = 1;
+  
+  for (let i = 0; i < arr.length; i++) {
+    multi *= arr[i];
+  }
+  return multi;
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	return num1 ** num2;
 };
 
-const factorial = function() {
-	
+const factorial = function(para) {
+  let fac = 1;
+  for (let i = 1; i <= para; i++){
+      fac *=i;
+  }
+  return fac;
 };
 
 // Do not edit below this line
