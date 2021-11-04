4af4308780ee390e561b215d4e33b06e523432fa
lupusalba <a145296@gmail.com>
2021-11-04 16:24:02 +0100

    finished plaindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,14 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let answer;
+    let original = str.toLowerCase().replace(/[^a-zA-Z]+/g, '').split("");
+    let arr = original.reverse();
 
+    if(original.toString() === arr.reverse().toString()) {
+        answer = true;
+    } else {
+        answer = false;
+    }
+    return answer;
 };
-
 // Do not edit below this line
 module.exports = palindromes;
