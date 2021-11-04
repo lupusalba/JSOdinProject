eda718d42c5562ea84ddcf598fd0876588f1c20d
lupusalba <a145296@gmail.com>
2021-11-04 18:09:33 +0100

    finished getTheTitle

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,11 @@
-const getTheTitles = function() {
+const getTheTitles = function(inArr) {
+    let outArr = [];
+    for(let i = 0; i < inArr.length; i++) {
+        outArr[i] = inArr[i].title;
+    }
 
+
+    return outArr;
 };
 
 // Do not edit below this line
